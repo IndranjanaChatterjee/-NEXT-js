69fdeb84ae652a4fbb64fbe8daeef19f2a447985
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-12 10:57:45 +0530

    route

--- my-app/app/login/page.js
+++ my-app/app/login/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='text-[5rem]'>
+    <div className='text-[5rem] font-bold min-h-[100vh]'>
       hhhhh
     </div>
   )
